8e33718bd70301f3467489c14891a9c3d957418d
Spawndemonic <spawndemonic@gmail.com>
2019-10-27 22:21:11 -0400

    need to create a loop for all the sources

--- dartplayground/gz.dart
+++ dartplayground/gz.dart
@@ -1,15 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'package:webfeed/webfeed.dart';
+import 'dart:io';
 
-void main() {
+
+void main() async {
   var client = new http.Client();
-
+  var file = File('source.txt');
+  var content;
+  if(await file.exists()){
+    content = await file.readAsString();
+  }
+content = content.split('\n');
+print(content[0]);
   // RSS feed
   client.get("http://rss.nytimes.com/services/xml/rss/nyt/World.xml").then((response) {
     return response.body;
   }).then((bodyString) {
     var channel = new RssFeed.parse(bodyString);
     print(channel.items[0].title);
-    return channel;
+    return channel.items;
   });
 }
