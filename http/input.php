ed9fb40ca3508158fbe5bf5317370d662554c970
Spawndemonic <spawndemonic@gmail.com>
2019-11-24 02:54:27 -0500

    working the main loop

--- http/input.php
+++ http/input.php
@@ -1,8 +1,16 @@
 <?php
+$wordList = fopen("wordlist.txt","r") or die("Unable to open file!");
 $string = $_POST["texta"];
 $spacesplit=explode("\n",$string);
-for($i=0;i<count($spacesplit);i++)
+$finalString = " ";
+for($i=0;i<count($spacesplit);$i++){
+while(!feof($wordList))
 {
+  $temp = str_split($wordList);
+  //when match is found
+  //$finalString .= ",".$wordList;
+ 
 }
-echo $spacesplit[1];
+}
+echo $finalString;
 ?>
