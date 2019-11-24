416df63a31417079c30069b3e3a48ef1470c91a3
spawndemonic <spawndemonic@gmail.com>
2019-11-24 00:45:24 -0500

    Add files via upload

---
+++ http/input.php
@@ -0,0 +1,8 @@
+<?php
+$string = $_POST["texta"];
+$spacesplit=explode("\n",$string);
+for($i=0;i<count($spacesplit);i++)
+{
+}
+echo $spacesplit[1];
+?>
---
+++ http/test.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+<h1>FUCK WADS</h1>
+<body>
+<form action = "input.php" method="post">
+<textarea rows"10" cols = "50"name="texta">
+
+</textarea>
+<input type="submit">
+</form>
+</body>
+</html>
