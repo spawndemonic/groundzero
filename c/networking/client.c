1e7fa9a3974c7bce51401f0b1be27f557898a97d
Ara <spawndemonic@gmail.com>
2020-01-20 21:55:40 -0500

    got a simple server working

--- c/networking/client.c
+++ c/networking/client.c
@@ -1,4 +1,8 @@
-// create socket
-  // connect to server
-//transmitt bruteforce
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/sockett.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include "tools.h"
+#define PORT 7980
--- c/networking/server.c
+++ c/networking/server.c
@@ -4,44 +4,45 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "tools.h"
 
 #define PORT 7890
 
 int main(void){
-  int sockfd,new_fd;
   struct sockaddr_in host_addr, client_addr;
   socklen_t sin_size;
   int recv_length=1, yes=1;
   char buffer[1024];
+  int socketfd, clientfd;
 
-  if ((sockfd = socket(PF_INET, SOCK_STREAM, 0))==-1)
-    fatal("in socket");
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1)
-    fatal("setting socket option SO_REUSEADDR");
   host_addr.sin_family = AF_INET;//assigning IPV4 address family
-  host_addr.sin_port = htons(PORT);
-  host_addr.sin_addr.s_addr = 0;
+  host_addr.sin_port = htons(PORT);//convert port number to netowork short
+  host_addr.sin_addr.s_addr = 0;//automatically assign IP
   memset(&(host_addr.sin_zero), '\0', 8);
 
-  if(bind(sockfd, (struct sockaddr *)$host_addr, sizeof(struct sockaddr))==-1)
+  if((socketfd = socket(AF_INET,SOCK_STREAM,0))==-1)
+    fatal("In Socket");
+
+  if(bind(socketfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))==-1)
      fatal("binding to socket");
-  if (listen(sockfd, 5) == -1)
+  if (listen(socketfd, 5) == -1)
     fatal("listening on socket");
 
   while(1){
     sin_size = sizeof(struct sockaddr_in);
-    new_sockfd = accept(sockfd, (struct sockaddr 8)&cline_addr, &sin_size);
-    if(new_sockfd == -1)
+    clientfd = accept(socketfd, (struct sockaddr *)&client_addr, &sin_size);
+    if(clientfd == -1)
       fatal("accepting connection");
     printf("server: got connection from %s prot %d\n",
         inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-    send(new_sockfd, "Hello, world!\n",13,0);
+    send(clientfd, "Hello, world!\n",13,0);
     while(recv_length > 0){
       printf("RECV: %d bytes\n", recv_length);
       dump(buffer, recv_length);
-      recv_length = recv(new_sockfd, &buffer, 1024, 0);
+      recv_length = recv(clientfd, &buffer, 1024, 0);
     }
-    close(new_sockfd);
+    shutdown(clientfd,2);
+    close(clientfd);
 
   }
   return 0;
