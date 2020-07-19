e29734037efe78498bff9922ee525722521eab07
Ara Ekmekjian <ekmekjian.ara@protonmail.com>
2020-07-19 15:31:55 -0400

    made java/gz a maven project to use shade

---
+++ java/gz/src/main/java/gz/App.java
@@ -0,0 +1,21 @@
+package gz;
+
+/**
+ * Hello world!
+ *
+ */
+//this is what the class name of the feedd is.
+// class="rpBJOHq2PR60pnwJlUyP0" 
+//for the item itself, each article has its own class but it's two words
+//example:class="_1poyrkZ7g36PawDueRza-J _2uazWzYzM0Qndpz5tFu3EX"
+// after looking at another item/article 
+//example:class="_1poyrkZ7g36PawDueRza-J _11R7M_VOgKO1RJyRSRErT3 "
+// the first part of the classs name doesn't change but the second part does for each article.
+//article are in <article></article>
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
