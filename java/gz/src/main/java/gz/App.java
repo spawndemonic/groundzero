a6354d391e438c78e875ebf9b94e297fdab1c337
Ara Ekmekjian <ekmekjian.ara@protonmail.com>
2020-10-25 18:52:09 -0400

    got a basic rectangle trying graphics now

--- java/gz/src/main/java/gz/App.java
+++ java/gz/src/main/java/gz/App.java
@@ -1,22 +1,42 @@
 package gz;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.*;
 
 
-public class App {
-    public static void main(String[]args){
-        JFrame frame =new JFrame();
-        JButton button = new JButton("fuckoff");
+public class App implements ActionListener {
+    //component declarations
+    JPanel panelA = new JPanel();
+    JPanel panelB = new JPanel();
+    JFrame frame =new JFrame();
+    Font butFont = new Font("serif", Font.BOLD,13);
+    JButton button = new JButton("fuckoff");
+    JButton button2 = new JButton("seriously");
+    JButton button3 = new JButton("fine whatever");
+    DisplayGraphics graphics = new DisplayGraphics();
+    App(){
+        button.setFont(butFont);
+        button3.addActionListener(this);
+        panelB.add(button);
+        panelB.add(button2);
+        panelB.add(button3);
+        panelA.add(panelB);
         // adds components like button to layout
-        frame.getContentPane().add(BorderLayout.SOUTH,button);
+        frame.getContentPane().add(BorderLayout.SOUTH,panelA);
+        frame.add(BorderLayout.CENTER,graphics);
         //set the size and make it visible
-        frame.setSize(300,300);
+        frame.setSize(500,300);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        
-        
     }
+    public void actionPerformed(ActionEvent event){
+        button3.setText("Trolled");
+    }
+    public static void main(String[]args){
+        App test = new App();
 
+    }
 }
---
+++ java/gz/src/main/java/gz/DisplayGraphics.java
@@ -0,0 +1,16 @@
+package gz;
+
+import java.awt.*;
+
+public class DisplayGraphics extends Canvas {
+    public DisplayGraphics(){
+        setSize(100,150);
+        setBackground(Color.BLACK);
+    }
+    public void paint(Graphics g){
+    g.drawOval(130,130,50,60);
+    g.fillOval(130,130,50,60);
+    g.setColor(Color.BLUE);
+    }
+
+}
