12f6a44eb2f741e312f4532e29c29d72157587c3
Ara Ekmekjian <ekmekjian.ara@protonmail.com>
2020-12-28 00:45:41 -0500

    added rust

---
+++ rust/gz/target/debug/deps/gz-8a429424a9ed055b.d
@@ -0,0 +1,5 @@
+/home/black/groundzero/rust/gz/target/debug/deps/gz-8a429424a9ed055b: src/main.rs
+
+/home/black/groundzero/rust/gz/target/debug/deps/gz-8a429424a9ed055b.d: src/main.rs
+
+src/main.rs:
---
+++ rust/gz/target/debug/gz.d
@@ -0,0 +1,1 @@
+/home/black/groundzero/rust/gz/target/debug/gz: /home/black/groundzero/rust/gz/src/main.rs
